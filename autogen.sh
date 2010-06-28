4b517cc11c44022f6b98fe0062c593b07d7b2bdf
Stefan Westerfeld <stefan@space.twc.de>
2010-06-28 12:20:10 +0200

    Be more verbose.

--- autogen.sh
+++ autogen.sh
@@ -3,4 +3,5 @@
 echo "Ensure: ChangeLog"
 test -e ChangeLog || TZ=GMT0 touch ChangeLog -t 190112132145.52 # automake *requires* ChangeLog
 
+echo "Running: autoreconf -i && ./configure $@"
 autoreconf -i -Wno-portability && ./configure "$@"
