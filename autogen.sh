99014f2086aa103b2e76519070e9def2467e3004
Stefan Westerfeld <stefan@space.twc.de>
2017-11-29 14:02:17 +0100

    BUILD: init automake using foreign to build without ChangeLog / README
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo "Ensure: ChangeLog"
-test -e ChangeLog || TZ=GMT0 touch ChangeLog -t 190112132145.52 # automake *requires* ChangeLog
-
 echo "Running: autoreconf -i && ./configure $@"
 autoreconf -i -Wno-portability && ./configure "$@"
